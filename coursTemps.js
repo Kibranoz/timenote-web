86969db84aa39ba98213040c5824df5f64eeb144
Louis <louisrcouture@protonmail.com>
2020-08-10 22:50:38 -0400

    ajout du viewport améliorer l'ajout des notes, reste a styler régler le problème du chronomètre

--- coursTemps.js
+++ coursTemps.js
@@ -4,13 +4,18 @@ var pauseTime = null
 class coursChrono {
     constructor(){
         this.initializeTimer = new Date().getTime()
+        this.actualTime = new Date().getTime()
     }
     printInitializeTimer(){
         console.log(this.initializeTimer);
     }
+    tic(){
+        this.actualTime +=1000
+    }
+
     calcTemps(){
-        var timenow = new Date().getTime();
-        var timeDifference = ((timenow - this.initializeTimer)/1000);
+        this.tic()
+        var timeDifference = ((this.actualTime - this.initializeTimer)/1000);
         this.seconds = timeDifference % 60
         this.minutes = (timeDifference / 60)%60
         this.hours = (timeDifference/3600);
@@ -18,12 +23,10 @@ class coursChrono {
         this.lastTime = Math.floor(this.hours).toString() + ":" + Math.floor(this.minutes).toString() + ":" + Math.floor(this.seconds).toString()
         return Math.floor(this.hours).toString() + ":" + Math.floor(this.minutes).toString() + ":" + Math.floor(this.seconds).toString() 
     }
-    pauseHandler(){
-        this.initializeTimer +=1000;
-    }
+
 
     addNewNoteField(){
-        $(".noteZone").append("<div>" + this.lastTime + "<input type = 'text'/></div>")
+        $(".noteZone").append("<div class='container'>" + this.lastTime + "<textarea class = 'form-control' type = 'text'></textarea></div>")
     }
 
     }
@@ -44,9 +47,6 @@ $("i.play").click(()=>{
 $('i.pause').click(()=>{
     clearInterval(playTime);
     playTime = null;
-    if (!pauseTime){
-    pauseTime = setInterval(()=>{chron.pauseHandler()},1000);
-    }
     $(".play").toggleClass("hidden");
     $(".pause").toggleClass("hidden");
 })
